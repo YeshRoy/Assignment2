a0bf8b9283f778b2f25037610540fdb986598d0d
SAWANT <sawantroy458@gmail.com>
2022-01-19 17:41:12 +0530

    update asignment

---
+++ src/com/technoelevate/pactice/TestStringFormatter.java
@@ -0,0 +1,12 @@
+package StringPrograms;
+
+public class TestStringFormatter {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringFormatter.reverseString("my name is niha"));  
+	    System.out.println(StringFormatter.reverseString("I am sonoo naidu")); 
+
+	}
+
+}
